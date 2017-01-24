b5891950f712be1b204a1cb53c8e876cebd1f015
Paolo Oliveira <paolocmo@gmail.com>
2017-01-24 01:16:34 -0300

    real files

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# go-modbus-cli
-A golang implementation of a Modbus TCP client for 16bit data
+# modbuscli
+
+A working Golang modbus client driver, implementing all 16 bits `Holding Register` functions:
+**0x03**(*Read Holding Register*), **0x06**(*Write Single Register*) and
+**0x10**(*Write Multiple Registers*) and handle their output. To use it, instance
+it and set the endpoint(modbus TCP server):
+
+```go
+ mt := new(ModBus)
+ mt.EndPoint = "127.0.0.1:502"
+```
+then use:
+- `res,_ := mt.WriteRegister(addr, value)` to write a single register **value** into **addr**.
+   Returns ([]int, error) where first position of []int is the address writen and the second is
+   the register itself;
+
+- `res,_ := mt.ReadHoldingRegister(addr, size)` to read **size** 16 bits register from **addr**.
+   Returns ([]int, error) where []int is an array of **size** values readen from **addr**;
+
+- `res,_ := mt.WriteRegisters(addr,[]int{values})` to write an array of **values** into **addr**.
+   Returns ([]int, error) where first position of []int is the addres written and the second is
+   the amount of registers written.
+
+
+### Enjoy
---
+++ modbus.go
@@ -0,0 +1,93 @@
+package modbuscli
+
+import (
+  "net"
+  "log"
+)
+
+const (
+  READHOLDINGREGISTER = 0x03
+  WRITEREGISTER = 0x06
+  WRITEREGISTERS = 0x10
+)
+
+func intTo2Byte(val int) ([]byte) { /* binary.BigEndian.PutUint16 */
+  b:= make([]byte,2)
+  b[0] = byte(val >> 8)
+  b[1] = byte(val)
+  return b
+}
+
+func byteTo16int(val []byte) (int) {
+  _ = val[1] // bounds check hint to compiler
+  return int(val[1]) | int(val[0])<<8
+}
+
+type ModBus struct {
+  EndPoint string
+  Addr byte
+  Code byte
+  Data []byte
+}
+
+func (m *ModBus) envelope() ([]byte) {
+  head := []byte{0x00, 0x00, 0x00, 0x00, 0x00, byte(len(m.Data) + 2), m.Addr, m.Code}
+  body := []byte{}
+  body = append(body, head...)
+  body = append(body, m.Data...)
+  return body
+}
+
+func (m *ModBus) WriteRegister(regAddr int, regVal int) ([]int, error){
+  m.Addr = 0x01
+  m.Code = WRITEREGISTER
+  m.Data = []byte{}
+  m.Data = append(m.Data, intTo2Byte(regAddr)...)
+  m.Data = append(m.Data, intTo2Byte(regVal)...)
+  tmp, err := m.send()
+  return []int{ byteTo16int(tmp[8:10]) , byteTo16int(tmp[10:12]) }, err
+}
+
+func (m *ModBus) ReadHoldingRegister(regAddr int, regSize int) ([]int, error) {
+  m.Addr = 0x01
+  m.Code = READHOLDINGREGISTER
+  m.Data = []byte{}
+  m.Data = append(m.Data, intTo2Byte(regAddr)...)
+  m.Data = append(m.Data, intTo2Byte(regSize)...)
+  tmp, err := m.send()
+  outp := []int{}
+  for i := 0; i < (regSize * 2); i+=2 {
+    outp = append(outp, []int{byteTo16int(tmp[9+i:9+i+2])}...)
+  }
+  return outp, err
+}
+
+func (m *ModBus) WriteRegisters(regAddr int, regVal []int) ([]int, error) {
+  m.Addr = 0x01
+  m.Code = WRITEREGISTERS
+  m.Data = []byte{}
+  m.Data = append(m.Data, intTo2Byte(regAddr)...)
+  m.Data = append(m.Data, intTo2Byte(len(regVal))...)
+  m.Data = append(m.Data, []byte{byte(len(regVal)*2)}...)
+  for _,v := range regVal{
+    m.Data = append(m.Data, intTo2Byte(int(v))...)
+  }
+  tmp,err := m.send()
+  return []int{ byteTo16int(tmp[8:10]) , byteTo16int(tmp[10:12]) }, err
+}
+
+func (m *ModBus) send() ([]byte, error) {
+  outp := make([]byte, 0x40)
+  addr, err := net.ResolveTCPAddr("tcp4", m.EndPoint); if err == nil {
+    c, err := net.DialTCP("tcp", nil, addr); if err == nil {
+      _, err = c.Write(m.envelope()); if err == nil {
+        _, err := c.Read(outp); if err == nil {
+          return outp, nil
+        }
+      }
+    }
+  }
+  return []byte{}, err
+}
+
+
